e7af9ad1d82af9105da41dc6417bdd9714a8aa01
IgorVodka <mail@igor.vodka>
2018-10-27 20:29:45 +0400

    Make fixes

--- src/main/java/com/company/FoundAnswer.java
+++ src/main/java/com/company/FoundAnswer.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class FoundAnswer {
+    Answer answer;
+    String shortAnswer;
+
+    public FoundAnswer(Answer answer, String shortAnswer) {
+        this.answer = answer;
+        this.shortAnswer = shortAnswer;
+    }
 }
